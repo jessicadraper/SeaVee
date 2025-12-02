5a07515540181ad9264dfabce7539a2a8ffba965
jessicadraper <jessicafaye.draper@gmail.com>
2025-12-02 14:57:14 +0100

    Initial Commit

---
+++ SeaVeeTests/SeaVeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeaVeeTests.swift
+//  SeaVeeTests
+//
+//  Created by Jessi Draper on 02.12.25.
+//
+
+import Testing
+@testable import SeaVee
+
+struct SeaVeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
